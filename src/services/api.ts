0df570048f908567dfc57e9cff5339e753a9199e
DRMO <inedumozey@gmail.com>
2025-11-27 09:30:05 +0100

    updated

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || "";
 
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: `${BASE_URL}/api/docs/v1`,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
